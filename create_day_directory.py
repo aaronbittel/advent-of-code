1b00406164a932bc0ed563b7e3ba4bff9a9731a4
Aaron Bittel <aaron@bittels.de>
2023-12-18 15:13:35 +0100

    Added a check in create_day_directory that if the directory is not empty, the files are not overritten

--- create_day_directory.py
+++ create_day_directory.py
@@ -34,9 +34,8 @@ if __name__ == "__main__":
 
     directory_name = create_directory(day_number)
 
-    if os.path.exists(directory_name):
-        print(f"Directory {directory_name} already exists.")
-        sys.exit(1)
-
-    copy_template(directory_name, day_number)
-    create_empty_txt_file(directory_name, file_name=f"puzzle_input_day{day_number}.txt")
+    if not os.listdir(directory_name):
+        copy_template(directory_name, day_number)
+        create_empty_txt_file(
+            directory_name, file_name=f"puzzle_input_day{day_number}.txt"
+        )
