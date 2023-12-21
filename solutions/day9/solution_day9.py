8e2d6662ffd57cbfcdeebbd77cb3195257dedee6
Aaron Bittel <aaron@bittels.de>
2023-12-21 18:21:11 +0100

    Solution for day 9 puzzle 2

--- solutions/day9/solution_day9.py
+++ solutions/day9/solution_day9.py
@@ -2,7 +2,7 @@ import time
 from functools import reduce
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 def load(file):
@@ -11,26 +11,33 @@ def load(file):
 
 
 def solve(p):
-    part1 = 0
+    part1, part2 = 0, 0
     histories = [list(map(int, row.split())) for row in p]
 
     for history in histories:
         adds = []
+        subs = []
         diff = history
         logging.debug(history)
         while not all(num == 0 for num in diff):
             adds.append(diff[-1])
+            subs.append(diff[0])
             diff = [right - left for left, right in zip(diff, diff[1:])]
             logging.debug(f"{diff} Sum: {sum(diff)}")
-        logging.debug(adds)
+        logging.debug(f"Adds: {adds}")
+        logging.debug(f"Subs: {subs}")
         logging.debug("-" * 20)
         part1 += sum(adds)
+        curr = 0
+        for sub in subs[::-1]:
+            curr = sub - curr
+        part2 += curr
 
-    return part1
+    return part1, part2
 
 
 if __name__ == "__main__":
     time_start = time.perf_counter()
-    solution = solve(load("puzzle_input_day9.txt"))
-    print(f"Part 1: {solution}")
+    sol_part1, sol_part2 = solve(load("puzzle_input_day9.txt"))
+    print(f"Part 1: {sol_part1}, Part2: {sol_part2}")
     print(f"Solved in {time.perf_counter() - time_start:.5f} Sec.")
