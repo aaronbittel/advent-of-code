70a77c4822b156ebc4800371fb3d307eee4312b5
Aaron Bittel <aaron@bittels.de>
2023-12-26 20:20:44 +0100

    Solution for day 18 puzzle 2

--- solutions/day18/solution_day18.py
+++ solutions/day18/solution_day18.py
@@ -9,6 +9,7 @@ DOWN = (0, 1)
 
 
 direction_map = {"R": RIGHT, "L": LEFT, "U": UP, "D": DOWN}
+direction_map2 = {"0": RIGHT, "1": DOWN, "2": LEFT, "3": UP}
 
 
 def load(file):
@@ -16,14 +17,20 @@ def load(file):
         return [row.strip().split() for row in f]
 
 
-def solve(p):
+def solve(p, part1):
     starting_pos = (0, 0)
     grid = []
     current_pos = starting_pos
-    for direction, length, _ in p:
+    for direction, length, color in p:
+        if not part1:
+            length = int(color[2:-2], 16)
+            dx, dy = direction_map2[color[-2]]
+        else:
+            length = int(length)
+            dx, dy = direction_map[direction]
+
         curr_x, curr_y = current_pos
-        dx, dy = direction_map[direction]
-        current_pos = (curr_x + int(length) * dx, curr_y + int(length) * dy)
+        current_pos = (curr_x + length * dx, curr_y + length * dy)
         grid.append(current_pos)
     polygon = shapely.Polygon(grid)
     return int(polygon.area + polygon.length / 2 + 1)
@@ -31,6 +38,9 @@ def solve(p):
 
 if __name__ == "__main__":
     time_start = time.perf_counter()
-    solution = solve(load("puzzle_input_day18.txt"))
-    print(f"Part 1: {solution}")
+    puzzle = load("puzzle_input_day18.txt")
+    sol_part1 = solve(puzzle, True)
+    sol_part2 = solve(puzzle, False)
+    print(f"Part 1: {sol_part1}")
+    print(f"Part 2: {sol_part2}")
     print(f"Solved in {time.perf_counter() - time_start:.5f} Sec.")
