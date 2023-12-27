005293cfffdbbd96682054740cb22ca0bd411cb6
Aaron Bittel <aaron@bittels.de>
2023-12-27 15:26:39 +0100

    Improved Solution for day 19 puzzle 1

--- solutions/day19/solution_day19.py
+++ solutions/day19/solution_day19.py
@@ -1,87 +1,59 @@
 import time
-from collections import namedtuple
-
-
-instruction = namedtuple("instruction", ["var", "condition", "target"])
+from collections import defaultdict
 
 
 def load(file):
     with open(file, "r") as f:
-        return [s.split("\n") for s in f.read().split("\n\n")]
+        return f.read()
+
+
+def parsing(p):
+    workflows, ratings = [row.split("\n") for row in p.split("\n\n")]
+    workflows2 = defaultdict(list)
+    for row in workflows:
+        name, conditions = row.split("{")
+        for condition in conditions.split(","):
+            if condition[-1] == "}":
+                con = (None, condition[:-1])
+            else:
+                con, target = condition.split(":")
+                var, op, num = con[0], con[1], int(con[2:])
+                con = ((var, op, num), target)
+            workflows2[name].append(con)
+
+    ratings = [eval(f"dict({rating[1:-1]})") for rating in ratings]
+    return workflows2, ratings
 
 
 def solve(p):
-    rules, parts = p[0], p[1]
-    rules_dict = create_rules(rules)
-    part_numbers = create_part_numbers(parts)
+    workflows, ratings = parsing(p)
 
-    return sum(
-        sum(part.values())
-        for part in part_numbers
-        if apply_rules(part, "in", rules_dict)
-    )
+    part1 = 0
+    for rating in ratings:
+        curr = "in"
+        while True:
+            for rule in workflows[curr]:
+                con, target = rule
+                if not con:
+                    curr = target
+                    break
+                var, op, num = con
+                if eval(f"{rating[var]}{op}{num}"):
+                    curr = target
+                    break
 
+            if curr == "A":
+                part1 += sum(rating.values())
+                break
 
-def apply_rules(part_num, current_rule, rules):
-    if current_rule == "R":
-        return False
-    elif current_rule == "A":
-        return True
+            if curr == "R":
+                break
 
-    start = rules[current_rule]
-    instructions = start["instructions"]
-    for inst in instructions:
-        var, condition, target = inst
-        number = part_num[var]
-        if condition.get("l", None):
-            check_condition = number < condition["l"]
-        else:
-            check_condition = number > condition["g"]
-
-        if check_condition:
-            # print(target)
-            return apply_rules(part_num, target, rules)
-    else:
-        # print(start["destination"])
-        return apply_rules(part_num, start["destination"], rules)
-
-
-def create_part_numbers(parts):
-    part_numbers = []
-    for part in parts:
-        my_dict = {}
-        for x in part[1:-1].split(","):
-            var, num = x.split("=")
-            my_dict[var] = int(num)
-        part_numbers.append(my_dict)
-    return part_numbers
-
-
-def create_rules(rules):
-    rules_list = {}
-    for r in rules:
-        name, instructions = r.split("{")
-        instructions = instructions.replace("}", "").split(",")
-        destination, instructions = instructions[-1], instructions[:-1]
-
-        instruction_list = []
-
-        for inst in instructions:
-            var = inst[0]
-            condition, target = inst[1:].split(":")
-            con = "g" if condition[0] == ">" else "l"
-            con_num = int(condition[1:])
-            instruct = instruction(var, {con: con_num}, target)
-            instruction_list.append(instruct)
-
-        ins = {"instructions": instruction_list, "destination": destination}
-
-        rules_list[name] = ins
-    return rules_list
+    return part1
 
 
 if __name__ == "__main__":
     time_start = time.perf_counter()
     solution = solve(load("puzzle_input_day19.txt"))
-    print(f"Part 1: {solution}")
+    print(f"Solution Part 1 & 2: {solution}")
     print(f"Solved in {time.perf_counter() - time_start:.5f} Sec.")
