79b0b9ac4ddc908d7aa4ae23ae864cc7e0773a3b
Aaron Bittel <aaron@bittels.de>
2025-12-29 15:17:57 +0100

    make 2021 a go package

---
+++ 2021/internal/common.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func GetFile() *os.File {
+	filename := GetFilename()
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func GetFilename() string {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <file>\n", os.Args[0])
+		os.Exit(1)
+	}
+	return os.Args[1]
+}
+
+func TimeIt[T any](f func() T) (T, time.Duration) {
+	start := time.Now()
+	res := f()
+	dur := time.Since(start)
+	return res, dur
+}
