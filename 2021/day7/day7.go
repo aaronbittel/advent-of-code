ab75e4982a477713a9eeee475795060e27bdfc63
Aaron Bittel <aaron@bittels.de>
2026-01-03 16:35:10 +0100

    use formula to calculate fuel cost

--- 2021/day7/day7.go
+++ 2021/day7/day7.go
@@ -19,88 +19,37 @@ func main() {
 	nums := parse(f)
 
 	part1, dur1 := common.TimeIt(func() int {
-		return part1(nums)
+		return solve(nums, true)
 	})
 	fmt.Printf("Part1: %d, took %s\n", part1, dur1)
 
 	part2, dur2 := common.TimeIt(func() int {
-		return part2(nums)
+		return solve(nums, false)
 	})
 	fmt.Printf("Part2: %d, took %s\n", part2, dur2)
 }
 
-func part2(nums []int) int {
-	mi, ma := slices.Min(nums), slices.Max(nums)
-
-	res := math.MaxInt
-	slices.Sort(nums)
-
-	cpy := make([]int, len(nums))
-
-	for i := mi; i <= ma; i++ {
-		copy(cpy, nums)
-		cost := calcFuel(cpy, i)
-		res = min(res, cost)
-	}
-
-	return res
-}
-
-func calcFuel(nums []int, level int) int {
-	var (
-		total   int
-		curCost = 1
-	)
-
-	for len(nums) != 0 {
-		var (
-			count int
-		)
-		for i, n := range nums {
-			if n == level {
-				continue
-			}
-			count++
-			if nums[i] > level {
-				nums[i]--
-			} else {
-				nums[i]++
-			}
-		}
-
-		total += curCost * count
-		curCost++
-
-		newNums := nums[:0]
-		for _, n := range nums {
-			if n == level {
-				continue
-			}
-			newNums = append(newNums, n)
-		}
-
-		nums = newNums
-	}
-
-	return total
-}
-
-func part1(nums []int) int {
+func solve(nums []int, constantCost bool) int {
 	mi, ma := slices.Min(nums), slices.Max(nums)
 
 	res := math.MaxInt
 
 	for i := mi; i <= ma; i++ {
-		res = min(res, calcFuelConstant(nums, i))
+		res = min(res, calcFuel(nums, i, constantCost))
 	}
 
 	return res
 }
 
-func calcFuelConstant(nums []int, level int) int {
+func calcFuel(nums []int, level int, constantCost bool) int {
 	var res int
 	for _, num := range nums {
-		res += int(math.Abs(float64(level - num)))
+		steps := int(math.Abs(float64(level - num)))
+		if constantCost {
+			res += steps
+		} else {
+			res += steps * (steps + 1) / 2
+		}
 	}
 	return res
 }
