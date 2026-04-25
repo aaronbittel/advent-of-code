30c99dd943c0fb1e26bc8e2abf450a5ee9205508
Aaron Bittel <aaron@bittels.de>
2026-04-25 11:17:21 +0200

    AOC2020: Day9 Part2

--- 2020/day9/day9.java
+++ 2020/day9/day9.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import java.io.IOException;
 
 import common.Common;
@@ -12,7 +13,7 @@ class Day9 {
 
     private static List<Long> parse(String filename) throws IOException {
         try (Stream<String> lines = Files.lines(Path.of(filename))) {
-            return lines.map(Long::parseLong).toList();
+            return lines.map(Long::parseLong).collect(Collectors.toList());
         }
     }
 
@@ -37,6 +38,29 @@ class Day9 {
         return nums.get(cur + windowSize);
     }
 
+    private static long solvePart2(List<Long> nums, long invalidNumber) {
+        int left = 0;
+        int right = 1;
+        long currentSum = nums.get(left) + nums.get(right);
+        while (currentSum != invalidNumber) {
+            if (currentSum < invalidNumber) {
+                right += 1;
+                currentSum += nums.get(right);
+            } else if (currentSum > invalidNumber) {
+                currentSum -= nums.get(left);
+                left += 1;
+            }
+        }
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        for (int i = left; i <= right; ++i) {
+            long val = nums.get(i);
+            if (val < min) min = val;
+            if (val > max) max = val;
+        }
+        return min + max;
+    }
+
     static void main(String[] args) {
         if (args.length < 1) {
             System.err.printf("Usage: java %s <input>%n", Day9.class.getSimpleName());
@@ -47,6 +71,10 @@ class Day9 {
         try {
             List<Long> nums = parse(filename);
             Common.time("Part1", () -> solvePart1(nums, 25));
+            Common.time("Part2", () -> {
+                long invalidNumber = solvePart1(nums, 25);
+                return solvePart2(nums, invalidNumber);
+            });
         } catch(IOException e) {
             System.err.println(e.getMessage());
             System.exit(1);
