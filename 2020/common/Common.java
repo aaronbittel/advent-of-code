f1022050c2c1313a1c5da8a9b911c595739bc957
Aaron Bittel <aaron@bittels.de>
2026-05-07 12:32:47 +0200

    feat: add timeParsing function

--- 2020/common/Common.java
+++ 2020/common/Common.java
@@ -10,4 +10,13 @@ public class Common {
         double seconds = (end - start) / 1_000_000_000.0;
         System.out.printf("%s: %s, took %.5f seconds%n", label, result, seconds);
     }
+
+    public static <T> T timeParsing(Supplier<T> task) {
+        long start = System.nanoTime();
+        T result = task.get();
+        long end = System.nanoTime();
+        double seconds = (end - start) / 1_000_000_000.0;
+        System.out.printf("Parsing took %.5f seconds%n", seconds);
+        return result;
+    }
 }
